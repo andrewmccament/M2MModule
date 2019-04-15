08df2498893c6497378ec55708ec7f05a4f6237e
inshalulhaq11 <inshalulhaq11@gmail.com>
2019-04-15 16:36:55 -0500

    Create M2MGet.py

---
+++ M2MGet.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+import time
+import M2MConnect
+import M2MSendCommand
+
+def connect(ser):
+    ret = M2MSendCommand.sendCommand("AT+CGDCONT?\r\n", ser)
+    print "AT+CGDCONT"
+    print ret
+    time.sleep(2)
+  
+  while True:
+   ret = sendCommand('AT+UHTTP=0,1,"google.com"\r\n', ser)
+   print "AT+UHTTP"
+   print ret
+   time.sleep(2)
+   ret = ser.write('AT+UHTTPC=0,1,"/","r"\r\n', ser)
+   print "AT+UHTTPC"
+   print ret
+   time.sleep(2)
+   ret = ser.write('AT+URDFILE="r"\r\n', ser)
+   print "AT+URDFILE"
+   print ret
+   time.sleep(2)
+   ret = sendCommand("AT\r\n", ser)
+   print "AT"
+   print ret
+   time.sleep(600)
