e9e00cedc49cff3c727cd6e331df29df8498ae19
inshalulhaq11 <inshalulhaq11@gmail.com>
2019-04-15 17:03:34 -0500

    Update M2MGet.py

--- M2MGet.py
+++ M2MGet.py
@@ -9,21 +9,21 @@ def get(ser):
     print ret
     time.sleep(2)
   
-while True:
-   ret = sendCommand('AT+UHTTP=0,1,"google.com"\r\n', ser)
-   print "AT+UHTTP"
-   print ret
-   time.sleep(2)
-   ret = ser.write('AT+UHTTPC=0,1,"/","r"\r\n', ser)
-   print "AT+UHTTPC"
-   print ret
-   time.sleep(2)
-   ret = ser.write('AT+URDFILE="r"\r\n', ser)
-   print "AT+URDFILE"
-   print ret
-   time.sleep(2)
-   ret = sendCommand("AT\r\n", ser)
-   print "AT"
-   print ret
-   time.sleep(600)
-   return True
+    while True:
+        ret = sendCommand('AT+UHTTP=0,1,"google.com"\r\n', ser)
+        print "AT+UHTTP"
+        print ret
+        time.sleep(2)
+        ret = ser.write('AT+UHTTPC=0,1,"/","r"\r\n', ser)
+        print "AT+UHTTPC"
+        print ret
+        time.sleep(2)
+        ret = ser.write('AT+URDFILE="r"\r\n', ser)
+        print "AT+URDFILE"
+        print ret
+        time.sleep(2)
+        ret = sendCommand("AT\r\n", ser)
+        print "AT"
+        print ret
+        time.sleep(600)
+        return True
