6920824a33bed6c1844f83e8a043995e5b29c9e4
andrewmccament <30536427+andrewmccament@users.noreply.github.com>
2019-04-24 17:37:48 -0500

    Updated M2MTest.py to reflect Drew's local changes

--- M2MTest.py
+++ M2MTest.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 import M2MModule
-import M2MConnect
 varTest = "test"
 ser = M2MModule.init()
 M2MModule.connect(ser)
-M2MModule.get(ser, "http://www.google.com")
+M2MModule.get(ser, "http://www.yahoo.com")
+M2MModule.post(ser, "https://postman-echo.com/post", "This is expected to be sent back as part of response body.")
+print "test is finished!"
