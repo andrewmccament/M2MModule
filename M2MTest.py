40fc3f5edab0714c37957b721bb2f61928fa6814
Andrew McCament <andrew@mccament.net>
2019-05-03 10:28:39 -0500

    fixed the URL

--- M2MTest.py
+++ M2MTest.py
@@ -3,6 +3,6 @@ import M2MModule
 ser = M2MModule.init()
 M2MModule.reboot(ser)
 M2MModule.connect(ser)
-M2MModule.get(ser, "https://www.yahoo.com/")
+M2MModule.get(ser, "www.yahoo.com")
 M2MModule.firmware(ser)
 M2MModule.post(ser, "postman-echo.com/post", "This is expected to be sent back as part of response body.")
